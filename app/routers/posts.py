a7c280873ddd1e53af47c387ffa87ff660dfc512
Shreya <shry.ds03@gmail.com>
2026-02-05 01:23:46 +0530

    all post retrieve endpoint made

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from typing import List
 from fastapi import APIRouter
 from entities import models
 from services.database import get_db
@@ -6,9 +7,11 @@ from entities.schemas import Post, PostCreate
 
 post_router=APIRouter()
 
-@post_router.get("/posts")
+@post_router.get("/posts", response_model= List[Post])
 async def get_posts():
-    return {"message":"this is ur post"}
+    db=get_db()
+    items= db.query(models.Posts).all()
+    return items
 
 @post_router.post("/post" ,status_code=HTTPStatus.CREATED)
 async def create_post(new_post:PostCreate):
